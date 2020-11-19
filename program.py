9bf6e64f5e5fb9a61eecf2eb054be224e37ebd71
timurtamerlan <fenix.born1673@gmail.com>
2020-11-19 20:39:33 +0300

    commit

--- program.py
+++ program.py
@@ -8,3 +8,7 @@ def hello_world():
 @app.route('/v2')
 def v2():
     return 'Second action'
+
+@app.route('/brichev')
+def brichev():
+    return 'Hello from CI with GitHub Actions by Brichev'
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,3 +10,8 @@ def test_v2(app, client):
     res = client.get('/v2')
     assert res.status_code == 200
     assert res.data == b"Second action"
+
+def test_brichev(app, client):
+    res = client.get('/brichev')
+    assert res.status_code == 200
+    assert res.data == b"Hello from CI with GitHub Actions by Brichev"
