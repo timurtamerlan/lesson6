3aa3a2906a77f51541f05e1186fc9e4cc9ae0ebf
timurtamerlan <fenix.born1673@gmail.com>
2020-11-19 18:42:00 +0300

    udate test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,7 +3,7 @@ import pytest
 def test_index(app, client):
     res = client.get('/')
     assert res.status_code == 200
-    assert res.data == b"Base action"
+    assert res.data == b"hello github"
 
 
 def test_v2(app, client):
